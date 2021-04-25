9c825ffe5810f98c6c853fe71a668cd9aac7a31c
cronycles <cronycles@gmail.com>
2021-04-25 11:03:36 +0200

    fixing and refactoring

--- README.md
+++ README.md
@@ -6,18 +6,15 @@ the project is hosted on
 https://www.crointhemorning.com/
 
 ## Project Requirements
-yo need to have **pip** installed on your system before running this project. See how to install it:
-### OSX 
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python3 get-pip.py
+yo need to have **nodejs** installed on your system before running this project.
    
 ## Setup
 - Go into project folder
 
 - install the requirements:
 
-    ```pip3 install --user -r ./requirements.txt```
-- then create a file called __config_user.py__ with the following content:
+    ```npm run restore```
+- then create a file called __config.js__ with the following content:
     ```
     #code of the telegram crobot
     telegramBotCode = 'YourTelegramBotCode'
@@ -32,17 +29,13 @@ yo need to have **pip** installed on your system before running this project. Se
     logFilePath= "./Logs/crobot.log"
     ```
 ## To launch bot 
-### OSX
-open terminal and launch this command
 
-```python3 start.py```
+### Use VScode?
+Start the bot with the Play __Run crobot__ command
+
+### OSX
+```npm run debug```
 
 ## TELEGRAM BOT COMMANDS
 To see if your bot exists, just type on your browser:
 https://api.telegram.org/bot\<bot-token>\/getMe
-
-
-## cronhob.. what is that?
-Sometimes, some hosting provider needs a ping in order to maintain alive the website.
-
-I have a php hosting and I created a cronjob that calls the file `cronjob.php` every x minutes and mantains alive my bot
--- crobot.js
+++ crobot.js
@@ -3,47 +3,54 @@ import config from './config.js'
 import messages from './messages.js'
 import fs from 'fs';
 import ytdl from 'ytdl-core';
-import BotUtils from './bot.utils';
+import BotUtils from './bot.utils.js';
 
 const bot = new Telegraf(config.telegramBotCode);
-const utils = BotUtils();
+const utils = new BotUtils();
 
 bot.start((ctx) => ctx.reply(messages.startReplayMessage));
 bot.help((ctx) => ctx.reply(messages.helpReplayMessage));
 bot.command('mychatid', (ctx) => {
-    const chatId = utils.getChatIdFromUtils();
+  try {
+    const chatId = utils.getChatIdFromContext(ctx);
     const message = messages.myChatIdReplayMessage.replace('{$chadId}', chatId);
     ctx.reply(message);
-});
 
-bot.url(/^https:\/\/youtu/i, (ctx) => {
-  let id = ctx.message.chat.id;
-	let url = ctx.message.text;
-  if(url.startsWith('https://youtu')) {
-    if(ytdl.validateURL(url)){
-      let aac_file = ytdl.getURLVideoID(url) + ".mp4";
-      ctx.reply("Downloading file to our server...");
-      const stream = ytdl(url, {quality: "highestaudio", filter: "audioonly"});
-      stream.pipe(fs.createWriteStream(aac_file));
-      stream.on('end', async () => {
-        ctx.reply("Uploading File...");
-        await ctx.replyWithAudio({source: "./" + aac_file});
-        fs.unlink("./" + aac_file, (err) => {
-          if (err) {
-            console.error(err)
-            return
-          }
-          
-          //file removed
-        })
-      });
-    }else{
-      ctx.reply("No Video Found");
-    }
+  } catch (e) {
+    ctx.reply("There was an error getting your chat id");
   }
 });
 
-
+bot.url(/^https:\/\/youtu/i, (ctx) => {
+  try {
+    let url = ctx.message.text;
+    if(url.startsWith('https://youtu')) {
+      if(ytdl.validateURL(url)){
+        let aac_file = ytdl.getURLVideoID(url) + ".mp4";
+        ctx.reply("Downloading file to our server...");
+        const stream = ytdl(url, {quality: "highestaudio", filter: "audioonly"});
+        stream.pipe(fs.createWriteStream(config.downloadDirectory + "/" + aac_file));
+        stream.on('end', async () => {
+          ctx.reply("Uploading File...");
+          await ctx.replyWithAudio({source: config.downloadDirectory + "/" + aac_file});
+          fs.unlink(config.downloadDirectory + "/" + aac_file, (err) => {
+            if (err) {
+              console.error(err)
+              return
+            }
+            
+            //file removed
+          })
+        });
+      } else{
+        ctx.reply("No Video Found");
+      }
+    }
+  }catch (e){
+    ctx.reply("There was an error transforming the youtube video");
+  }
+  
+});
 
 bot.on('text', (ctx) => {
 	ctx.reply("if you don't know what to do send me ask me with the /help command");
